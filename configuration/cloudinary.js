1c6e56b00b8ee1ec4a92c3264f8682af8efe0fb0
AhmedEhabH <ahmed.ehab.h@gmail.com>
2020-06-19 01:17:58 +0200

    upload to cloudinary

---
+++ configuration/cloudinary.js
@@ -0,0 +1,25 @@
+
+// const multer = require("multer");
+var cloudinary = require('cloudinary').v2;
+// const cloudinaryStorage = require("multer-storage-cloudinary");
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+function upload(url){
+    cloudinary.uploader.upload(url, (error, result)=>{
+        console.log('result');
+        console.log(result);
+        console.log('error');
+        console.log(error);
+        return result;
+    });
+}
+
+module.exports.uploadImage = (url) => {
+    return upload(url)
+}
--- controllers/savePictures.js
+++ controllers/savePictures.js
@@ -1,38 +1,6 @@
 let https = require('https');
 let fs = require('fs');
-
-const { Storage } = require('@google-cloud/storage');
-const storage = new Storage();
-
-
-async function uploadFile() {
-    const options = {
-        version: 'v4',
-        action: 'write',
-        expires: Date.now() + 15 * 60 * 1000, // 15 minutes
-        contentType: 'application/octet-stream',
-    };
-
-    // Uploads a local file to the bucket
-    const [url] = await storage.bucket(bucketName).upload(filename, {
-        // Support for HTTP requests made with `Accept-Encoding: gzip`
-        gzip: true,
-        // By setting the option `destination`, you can change the name of the
-        // object you are uploading to a bucket.
-        metadata: {
-            // Enable long-lived HTTP caching headers
-            // Use only if the contents of the file will never change
-            // (If the contents will change, use cacheControl: 'no-cache')
-            cacheControl: 'public, max-age=31536000',
-        },
-    }).getSignedUrl(options);;
-
-    console.log(`${filename} uploaded to ${bucketName}.\nURL:`);
-    console.log(url)
-    return new Promise((resolve, reject) => {
-        resolve(url);
-    })
-}
+let upload = require('../configuration/cloudinary');
 
 function savePhoto(url, path) {
     // return new Promise((resolve, reject) => {
@@ -42,8 +10,7 @@ function savePhoto(url, path) {
     //     });
     // });
 
-    return uploadFile().catch(console.error);
-    // [END storage_upload_file]
+    return upload.uploadImage(url).url;
 }
 
 module.exports.uploadedPhoto = () => {
