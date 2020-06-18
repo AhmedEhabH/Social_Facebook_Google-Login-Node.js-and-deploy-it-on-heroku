04308e5404164467ab9e2cc69dfc561ad1ba6255
AhmedEhabH <ahmed.ehab.h@gmail.com>
2020-06-19 00:00:46 +0200

    try to upload on google storage

--- controllers/savePictures.js
+++ controllers/savePictures.js
@@ -1,13 +1,49 @@
 let https = require('https');
 let fs = require('fs');
 
-function savePhoto(url, path){
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage();
+
+
+async function uploadFile() {
+    const options = {
+        version: 'v4',
+        action: 'write',
+        expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+        contentType: 'application/octet-stream',
+    };
+
+    // Uploads a local file to the bucket
+    const [url] = await storage.bucket(bucketName).upload(filename, {
+        // Support for HTTP requests made with `Accept-Encoding: gzip`
+        gzip: true,
+        // By setting the option `destination`, you can change the name of the
+        // object you are uploading to a bucket.
+        metadata: {
+            // Enable long-lived HTTP caching headers
+            // Use only if the contents of the file will never change
+            // (If the contents will change, use cacheControl: 'no-cache')
+            cacheControl: 'public, max-age=31536000',
+        },
+    }).getSignedUrl(options);;
+
+    console.log(`${filename} uploaded to ${bucketName}.\nURL:`);
+    console.log(url)
     return new Promise((resolve, reject) => {
-        https.get(url, (res) => {
-            res.pipe(fs.createWriteStream(path));
-            resolve({ path: path.substr(8) });
-        });
-    });
+        resolve(url);
+    })
+}
+
+function savePhoto(url, path) {
+    // return new Promise((resolve, reject) => {
+    //     https.get(url, (res) => {
+    //         res.pipe(fs.createWriteStream(path));
+    //         resolve({ path: path.substr(8) });
+    //     });
+    // });
+
+    return uploadFile().catch(console.error);
+    // [END storage_upload_file]
 }
 
 module.exports.uploadedPhoto = () => {
